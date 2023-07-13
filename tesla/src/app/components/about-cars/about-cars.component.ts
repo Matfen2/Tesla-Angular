d89bfe8ab2f6c6007d986c0165412aabca0fdedf
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2023-07-13 14:14:45 +0200

    next version

--- tesla/src/app/components/about-cars/about-cars.component.ts
+++ tesla/src/app/components/about-cars/about-cars.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+interface about {
+  id: number;
+  video: string;
+}
 @Component({
   selector: 'app-about-cars',
   templateUrl: './about-cars.component.html',
   styleUrls: ['./about-cars.component.css']
 })
+
 export class AboutCarsComponent {
 
 }
